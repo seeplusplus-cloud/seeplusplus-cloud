03dfbac481a34c216922e024ea00a18535c9db4d
seeplusplus-cloud <76935501+seeplusplus-cloud@users.noreply.github.com>
2021-02-17 20:54:32 +0530

    Updated bst.cpp to contain deletion of nodes

--- bst.cpp
+++ bst.cpp
@@ -1,31 +1,63 @@
 #include<iostream>
 using namespace std;
 
-struct search{int data;search *left,*right;search(){left=NULL;right=NULL;}};
+struct Node{int data;Node *left,*right;};
 
-void insert(search **root,int a)
+Node *findMin(Node *root){while(root->left!=NULL){root=root->left;}return root;}
+Node *findPar(Node *root,Node *q)
 {
-   search *t=new search;
-   if(*root==NULL){t->data=a;*root=t;}
-   else
-   {
-       if((*root)->data>a){insert(&((*root)->left),a);}
-       else{insert(&((*root)->right),a);}
-   }
-   
-   return;
-   
+	if(root==q){return NULL;}
+	if(root->left==q||root->right==q){return root;}
+	if(root->data<q->data){return findPar(root->right,q);}
+	if(root->data>q->data){return findPar(root->left,q);}
 }
 
-void inorder(search *root)
+void insertion(Node **root,int dat)
 {
-    if(root==NULL)return;
-    inorder(root->left);
-    cout<<root->data<<" ";
-    inorder(root->right);
+	if((*root)==NULL)
+	{
+		(*root)=new Node;
+		(*root)->data=dat;
+		(*root)->left=NULL;
+		(*root)->right=NULL;
+		return;
+	}
+	if((*root)->data>dat){insertion(&((*root)->left),dat);}
+	else{insertion(&((*root)->right),dat);}
 }
 
-void printTree(search *root,int l)
+void deletion(Node **root,int query)
+{
+	if((*root)==NULL){return;}
+	if((*root)->data>query){deletion(&((*root)->left),query);return;}
+	if((*root)->data<query){deletion(&((*root)->right),query);return;}
+	Node *rtemp=*root;
+	if(rtemp->left==NULL&&rtemp->right==NULL)
+	{
+		*root=NULL;
+		delete(rtemp);
+		return;
+	}
+	if(rtemp->right==NULL)
+	{
+		*root=rtemp->left;
+		delete(rtemp);
+		return;
+	}
+	if(rtemp->left==NULL)
+	{
+		*root=rtemp->right;
+		delete(rtemp);
+		return;
+	}
+	Node *inor=findMin(rtemp->right);
+	int rep=inor->data;
+	Node *par=findPar(rtemp,inor);
+	deletion(&par,rep);
+	(*root)->data=rep;
+} 
+
+void printTree(Node *root,int l)
 {
     if(root==NULL){return;}
     l+=10;
@@ -38,17 +70,26 @@ void printTree(search *root,int l)
 
 int main()
 {
-    search *root=NULL;
-    int a[20],n;
-    cout<<"enter size ";cin>>n;
-    cout<<"enter elements to enter in bst ";
-    for(int i=0;i<n;i++){cin>>a[i];}
-    for(int i=0;i<n;i++)
-    {
-        insert(&root,a[i]);
-    }
-    //inorder(root);
-    printTree(root,0);
-    cout<<"\r\n";
-    return 0;
+	Node *root=NULL;
+	char c='y';
+	int op,q;
+	while(c=='y'||c=='Y')
+	{
+		system("clear");
+		cout<<"enter op 1:ins 2:del : ";cin>>op;
+		if(op==1){
+			cout<<"enter data : ";cin>>q;
+			insertion(&root,q);
+		}
+		else if(op==2){
+			cout<<"enter query : ";cin>>q;
+			deletion(&root,q);
+		}
+		else{
+			cout<<"no op";
+		}
+		printTree(root,0);
+		cout<<"\ndo you want to op more : ";cin>>c;
+	}
+	return 0;	
 }
